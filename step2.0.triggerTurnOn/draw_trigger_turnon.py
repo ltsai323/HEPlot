3635f3b0c12db50b6dd2b0f880afa3f242d8b1ad
ltsai323 <johnson20050@gmail.com>
2024-08-22 17:32:10 +0800

    Load TGraphAsymError

--- step2.0.triggerTurnOn/draw_trigger_turnon.py
+++ step2.0.triggerTurnOn/draw_trigger_turnon.py
@@ -7,6 +7,11 @@ import mplhep as hep
 from uncertainties import ufloat, unumpy
 import HistSet
 
+def GetArgs_InputAndOutput(argv):
+    if len(argv) == 2:
+        return argv[1]
+    return 'test.yaml'
+
 
 
 
@@ -19,45 +24,114 @@ def take_ratio(hNUMERATOR:HistSet.HistSet, hDENOMINATOR:HistSet.HistSet) -> tupl
     value_denominator = HistSet.GetBinData(hDENOMINATOR)
     ratio = [ u/d if d != 0 else ufloat(0,0) for u,d in zip(value_numerator,value_denominator) ]
 
-    my_ratio = []
-    my_ratio_err = []
-    #for u,d in zip(value_numerator, value_denominator):
-    #    vu = u.nominal_value
-    #    eu = u.std_dev
-    #    vd = d.nominal_value
-    #    ed = d.std_dev
-    #    my_ratio.append(vu/vd)
-    #    #my_ratio_err.append( 
-        
     return ( unumpy.nominal_values(ratio),unumpy.std_devs(ratio) )
 
-def trigger_turn_on(hNUMERATOR:HistSet.HistSet, hDENOMINATOR:HistSet.HistSet,
-                    xLABEL, yLABEL, ySCALE
+
+def draw_ratio_plot(ax,
+                    hNUMERATOR:HistSet.HistSet, hDENOMINATOR:HistSet.HistSet,
+                    label:str, drawYerr=False):
+    ratio_value, ratio_error = take_ratio(hNUMERATOR,hDENOMINATOR)
+    bin_center = HistSet.GetBinCenter(hNUMERATOR)
+    bin_width  = HistSet.GetBinWidth(hNUMERATOR)
+    
+    xVAL = bin_center
+    xERR = bin_width
+
+    yVAL = ratio_value
+    yERR = ratio_error if drawYerr else None
+    ax.errorbar( xVAL, yVAL, xerr=xERR, yerr=yERR, label=label )
+    
+def trigger_turn_on(listOFdrawableFUNC:list,
+                      xLABEL, yLABEL, ySCALE, yRANGE:tuple=(),
                     ):
+    '''
+        Args:
+            listOFdrawableFUNC is a list containing a tuple, first is callback function, the second one is the xargs
+            listOFdrawableFUNC = [
+                ( func1, xargs1 ),
+                ( func2, xargs2 ),
+            ]
+    '''
+
+    #plt.clf()
     hep.style.use(hep.style.ROOT) # For now ROOT defaults to CMS
     hep.style.use("CMS") # string aliases work too
     fig, ax = plt.subplots(figsize=(8,8))
 
-    ratio_value, ratio_error = take_ratio(hNUMERATOR,hDENOMINATOR)
-    bin_center = HistSet.GetBinCenter(hNUMERATOR)
-    bin_width  = HistSet.GetBinWidth(hNUMERATOR)
+    for drawbleFUNC, funcPARAMETERdict in listOFdrawableFUNC:
+        drawbleFUNC(ax, **funcPARAMETERdict)
 
-    #ax.errorbar( bin_center, ratio_value, xerr=bin_width, yerr=ratio_error, label=hNUMERATOR.desc, **HistSet.PLOTSTYLE_STAR1 )
-    ### trigger turn on plot do not contains statistic uncertainties
-    ax.errorbar( bin_center, ratio_value, xerr=bin_width, label=hNUMERATOR.desc, **HistSet.PLOTSTYLE_DATA )
     ax.set_xlabel(xLABEL)
     ax.set_ylabel(yLABEL)
-    ax.set_ylim(0.,1.4)
+    if len(yRANGE):
+        ax.set_ylim( yRANGE[0], yRANGE[1] )
     ax.legend()
     plt.suptitle('')
     plt.grid(True, which='major', axis='y')
 
     hep.cms.label(ax=ax, **INFO_CMS_PRELIMIILARY_UL2016POSTVFP)
 
-if __name__ == "__main__":
+    return fig, ax
 
-# Open the ROOT file
-    file = uproot.open("result.root")
+class DrawableEntry:
+    def __init__(self, entry:dict):
+        try:
+            self.num = entry['num']
+            self.den = entry['den']
+            self.root = entry['root']
+            self.label = entry['label']
+        except KeyError as e:
+            mesg = f'[DrawableEntry] "num", "den", "root", "label" are required in yaml file'
+            print('\n\n' + '*' * len(mesg) )
+            print(mesg)
+            print('*' * len(mesg) + '\n\n')
+            raise KeyError(e)
+class YAMLEntry:
+    def __init__(self, entry:dict):
+        try:
+            self.fig_name = entry['figNAME']
+            self.y_title = entry['yTITLE']
+            self.ratio_plots = [ DrawableEntry(e) for e in entry['ratioPLOTs'] ]
+        except KeyError as e:
+            mesg = f'[YAMLEntry] "figNAME", "yTITLE", "ratioPLOTs" are required in yaml file'
+            print('\n\n' + '*' * len(mesg) )
+            print(mesg)
+            print('*' * len(mesg) + '\n\n')
+            raise KeyError(e)
+
+    def __str__(self):
+        return f'Configs for entry "{self.y_title}"'
+    def __repr__(self):
+        return self.__str__()
+def GetYAMLConfigs(yamlFILE):
+    '''
+    output yaml format with 
+    out = [
+        { 'figNAME': 'hi.jpg', 'yTITLE': 'test', 
+            'ratioPLOTs': [
+                { 'num': 'Range7_passingBothJetHLT7', 'den': 'Range7_passingJetHLT', 'label': 'jjj', 'root': 'a.root', },
+                { 'num': 'Range7_passingBothJetHLT7', 'den': 'Range7_passingJetHLT', 'label': 'kkk', 'root': 'b.root', },
+                ]
+        },
+        { 'figNAME': 'hi.jpg', 'yTITLE': 'test', 
+            'ratioPLOTs': [
+                { 'num': 'Range7_passingBothJetHLT7', 'den': 'Range7_passingJetHLT', 'label': 'jjj', 'root': 'a.root', },
+                { 'num': 'Range7_passingBothJetHLT7', 'den': 'Range7_passingJetHLT', 'label': 'kkk', 'root': 'b.root', },
+                ]
+        },
+    ]
+    '''
+    import yaml
+    with open(yamlFILE, 'r') as fIN:
+        return [ YAMLEntry(entry) for entry in yaml.safe_load(fIN) ]
+
+if __name__ == "__main__":
+    import sys
+    inFILE = GetArgs_InputAndOutput(sys.argv)
+    entries = GetYAMLConfigs( inFILE )
+    
+
+
 
 # Access the 'expdata' directory
     def getHistSet(tFILE, var, **xargs):
@@ -65,40 +139,36 @@ if __name__ == "__main__":
         xargs['orig_obj'] = h
         return HistSet.HistSet(h.to_hist(), **xargs)
 
-    absolute = [
-            #{ 'num': 'Range0_passingBothJetHLT0','den': 'Range0_passingJetHLT', 'yTITLE':'Efficiency of jjjj', },
-            #{ 'num': 'Range1_passingBothJetHLT1','den': 'Range1_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range2_passingBothJetHLT2','den': 'Range2_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range3_passingBothJetHLT3','den': 'Range3_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range4_passingBothJetHLT4','den': 'Range4_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range5_passingBothJetHLT5','den': 'Range5_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon125', },
-            #{ 'num': 'Range6_passingBothJetHLT6','den': 'Range6_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon150', },
-            { 'num': 'Range7_passingBothJetHLT7','den': 'Range7_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon175', },
-            #{ 'num': 'Range8_passingBothJetHLT7','den': 'Range8_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon175', },
-            ]
-    var_looping_info = [
-            #{ 'num': 'Range0_passingBothJetHLT0','den': 'Range0_passingJetHLT', 'yTITLE':'Efficiency of jjjj', },
-            #{ 'num': 'Range1_passingBothJetHLT1','den': 'Range1_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range2_passingBothJetHLT2','den': 'Range2_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range3_passingBothJetHLT3','den': 'Range3_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range4_passingBothJetHLT4','den': 'Range4_passingJetHLT', 'yTITLE':'Efficiency of linear', },
-            #{ 'num': 'Range5_passingBothJetHLT5','den': 'Range5_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon125', },
-            #{ 'num': 'Range6_passingBothJetHLT6','den': 'Range6_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon150', },
-            { 'num': 'Range7_passingBothJetHLT7','den': 'Range7_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon175', },
-            #{ 'num': 'Range8_passingBothJetHLT7','den': 'Range8_passingJetHLT', 'yTITLE':'Efficiency of HLT_Photon175', },
-            ]
 
-    for var_info in var_looping_info:
-        h_numerator   = getHistSet(file, var_info['num'], desc='Jet HT')
-        h_denominator = getHistSet(file, var_info['den'])
-        trigger_turn_on(h_numerator, h_denominator,
-            xLABEL='$p_{T}^{\gamma}$', yLABEL=var_info['yTITLE'], ySCALE='linear'
-        )
-        figNAME = var_info.get('figNAME','')
+    fig_configurations = {
+            'xLABEL': '$E^{\gamma}_{T}$ (GeV)',
+            'yLABEL': 'modification required',
+            'ySCALE': 'linear',
+            'yRANGE': (0.,1.4),
+    }
+    for entry in entries:
+        drawing_contents = []
+        for drawable_entry in entry.ratio_plots:
+            rootFILE = uproot.open(drawable_entry.root)
+            h_numerator   = getHistSet(rootFILE, drawable_entry.num, desc='aaaaa') # desc need to be removed
+            h_denominator = getHistSet(rootFILE, drawable_entry.den, desc='aaaaa')
+            drawable_parameters = {
+                'hNUMERATOR': h_numerator, 'hDENOMINATOR': h_denominator, 
+                'label': drawable_entry.label,
+                'drawYerr': False
+            }
+            drawing_contents.append( (draw_ratio_plot, drawable_parameters) )
+            rootFILE.close()
+
+        fig_configurations['yLABEL'] = entry.y_title
+        fig, ax = trigger_turn_on( drawing_contents, **fig_configurations)
+        figNAME = entry.fig_name
         if figNAME:
-            ple.savefig(figNAME)
+            plt.savefig(figNAME)
             print(f'[SavedOutput] {figNAME}')
         else:
             plt.show()
             current_figsize = fig.get_size_inches()
             print(f"[AdjustedFigureSize] {current_figsize[0]} inches x {current_figsize[1]} inches")
+        #plt.cla()
+
